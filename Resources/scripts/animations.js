37538ae1dedd2dddeb4cd03a39fa1b867bebcbd7
Shachar <cocoecco@yahoo.com>
2014-08-31 16:39:59 -0700

    dsa

--- Resources/scripts/animations.js
+++ Resources/scripts/animations.js
@@ -1,11 +1,19 @@
-    function enterFromLeft() {
-        setTimeout(function(){
-            $( "#fullPageDiv" ).animate({
-                left: "-200%"
-            }, 500, function() {
-             // Animation complete.
-            }); 
-        }, 1000);
-        
+function enterFromLeft() {
+    setTimeout(function(){
+        $( "#fullPageDiv" ).animate({
+            left: "-200%"
+        }, 500, function() {
+         // Animation complete.
+        }); 
+    }, 1000);
+}   
 
-    }   
+function showPageOne() {
+    setTimeout(function(){
+        $( "#fullPageDiv" ).animate({
+            left: "-90%"
+        }, 500, function() {
+         // Animation complete.
+        }); 
+    }, 20); 
+}
