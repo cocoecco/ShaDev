6b57dd61cb41caf21d101bdca92256176234b534
Shachar <cocoecco@yahoo.com>
2014-08-31 17:51:29 -0700

    dasd

--- Resources/scripts/animations.js
+++ Resources/scripts/animations.js
@@ -9,6 +9,8 @@ function enterFromLeft() {
 }   
 
 function showPageOne() {
+    rotateAnimation(protoImg, 0.4);
+    
     setTimeout(function(){
         $( "#fullPageDiv" ).animate({
             left: "-100%"
@@ -49,4 +51,40 @@ function centerFromPageThree() {
         }); 
     }, 20); 
 }
+
+
+
+
+
+
+function rotateAnimation(el,speed){
+var elem = document.getElementById(el);
+if(navigator.userAgent.match("Chrome")){
+elem.style.WebkitTransform = "rotate("+degrees+"deg)";
+} else if(navigator.userAgent.match("Firefox")){
+elem.style.MozTransform = "rotate("+degrees+"deg)";
+} else if(navigator.userAgent.match("MSIE")){
+elem.style.msTransform = "rotate("+degrees+"deg)";
+} else if(navigator.userAgent.match("Opera")){
+elem.style.OTransform = "rotate("+degrees+"deg)";
+} else {
+elem.style.transform = "rotate("+degrees+"deg)";
+}
+looper = setTimeout('rotateAnimation(\''+el+'\','+speed+')',speed);
+degrees++;
+if(degrees > 359){
+degrees = 1;
+}
+document.getElementById("status").innerHTML = "rotate("+degrees+"deg)";
+}
+
+
+
+
+
+
+
+
+
+
 
